896c583f7b19f2baf24ff0a34665e2e0120a4136
daemon32 <root@localhost>
2017-07-09 10:06:23 -0700

    My changes

--- cpu.h
+++ cpu.h
@@ -1,17 +1,36 @@
+/*    This file is part of tsctest-linux.
+ *
+ *    tsctest-linux is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    tsctest-linux is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with tsctest-linux.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef CPU_H
 #define CPU_H
 
-int cpu_rdtsc();
+/* Copyright daemon32 2017 */
+struct cpu_tsc_struct
+{
+	int returnval;
+	int returnval_vmexit;
+	unsigned long long avg;
+	unsigned long long vmexit_avg;
+} cpu_tsc_avg;
 
-int cpu_rdtsc_force_vmexit();
+static inline unsigned long long rdtsc_diff();
+static inline unsigned long long rdtsc_diff_vmexit();
+/* End Copyright daemon32 2017 */
 
-int cpu_hv();
-
-void cpu_write_vendor(char *);
-void cpu_write_hv_vendor(char *);
-void cpu_write_brand(char *);
-
-int cpu_known_vm_vendors();
+void cpu_rdtsc();
+void cpu_rdtsc_force_vmexit();
 
 #endif
